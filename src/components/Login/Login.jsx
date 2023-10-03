59ad7e7d78e97a20c9b744039e082e7b436ddf14
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-10-03 20:05:25 +0600

    learned how to register or login with email & password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,48 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
+import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 const Login = () => {
+    const [loginError, setLoginError] = useState("");
+    const [loginSucces, setLoginSucces] = useState("");
+    const [isShow, setIsShow] = useState(false);
+
+    const emailRef = useRef(null);
+
     const handleLogIn = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const pass = e.target.password.value;
-        console.log(email, pass);
+
+        // resetting status
+        setLoginError("");
+        setLoginSucces("");
 
         signInWithEmailAndPassword(auth, email, pass)
-        .then(res => console.log(res.user))
-        .catch(err => console.log(err));
+            .then(res => {
+                console.log(res.user);
+                setLoginSucces("Login succesfull!");
+            })
+            .catch(err => {
+                console.log(err);
+                setLoginError(err.message);
+            });
+    }
+
+    const handleResetPassword = () => {
+        const email = emailRef.current.value;
+
+        if (!email) {
+            alert("Please enter your email!");
+            return;
+        }
+
+        // resetting password
+        sendPasswordResetEmail(auth, email)
+            .then(() => alert("Email sent!"))
+            .catch(err => alert(err.message));
     }
 
     return (
@@ -27,20 +59,30 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input required type="email" name="email" placeholder="email" className="input input-bordered" />
+                                <input required ref={emailRef} type="email" name="email" placeholder="email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input required type="password" name="password" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <div className="relative form-control">
+                                    <input required type={isShow ? "text" : "password"} name="password" placeholder="password" className="input input-bordered" />
+                                    <span onClick={() => setIsShow(!isShow)} className="absolute h-full flex items-center top-0 right-3 cursor-pointer">{isShow ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>}</span>
+                                </div>
+                                <label>
+                                    <p href="" className="label-text-alt text-base">New to this website? <Link to="/register"><a href="" className="link link-hover">Register</a></Link></p>
+                                    <p onClick={handleResetPassword} className="link link-hover">Forgot password?</p>
                                 </label>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Login</button>
                             </div>
+                            {
+                                loginSucces && <div><p className="text-green-500">{loginSucces}</p></div>
+                            }
+                            {
+                                loginError && <div><p className="text-red-500">{loginError}</p></div>
+                            }
                         </div>
                     </form>
                 </div>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,16 +1,43 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
+import { useState } from "react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+    const [isShow, setIsShow] = useState(false);
+    const [registerError, setRegisterError] = useState("");
+    const [registerSucces, setRegisterSucces] = useState("");
+
     const handleRegister = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const pass = e.target.password.value;
-        console.log(email, pass);
+        const terms = e.target.terms.checked;
+        
+        console.log(email, pass, terms);
+
+        // resetting status
+        setRegisterError("");
+        setRegisterSucces("");
+
+        if (pass.length < 6) {
+            setRegisterError("Your password must be at least 6 characters or more!");
+            return;
+        }
+
+        if (!terms) {
+            setRegisterError("Please accept our terms and conditions!");
+            return;
+        }
 
         createUserWithEmailAndPassword(auth, email, pass)
-        .then(res => console.log(res.user))
-        .catch(err => console.log(err));
+            .then((res) => {
+                sendEmailVerification(res.user);
+                alert("Verification email sent!");
+                setRegisterSucces("User created succesfully!");
+            })
+            .catch(err => setRegisterError(err.message));
     }
 
     return (
@@ -33,14 +60,25 @@ const Register = () => {
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input required type="password" name="password" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
+                                <div className="relative form-control">
+                                    <input required type={isShow ? "text" : "password"} name="password" placeholder="password" className="input input-bordered" />
+                                    <span onClick={() => setIsShow(!isShow)} className="absolute h-full flex items-center top-0 right-3 cursor-pointer">{isShow ? <FaEyeSlash className="text-xl"></FaEyeSlash> : <FaEye className="text-xl"></FaEye>}</span>
+                                </div>
+                                <p href="" className="label-text-alt text-base">Already have an acount? <Link to="/login"><a href="" className="link link-hover">Log In</a></Link></p>
+                                <div className="mt-3">
+                                    <input type="checkbox" name="terms" id="terms" />
+                                    <label className="ml-2" htmlFor="terms">Accept our terms & conditions</label>
+                                </div>
                             </div>
                             <div className="form-control mt-6">
                                 <button className="btn btn-primary">Creat acount</button>
                             </div>
+                            {
+                                registerSucces && <div><p className="text-green-500">{registerSucces}</p></div>
+                            }
+                            {
+                                registerError && <div><p className="text-red-500">{registerError}</p></div>
+                            }
                         </div>
                     </form>
                 </div>
